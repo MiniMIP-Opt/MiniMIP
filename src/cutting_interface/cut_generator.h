8974d860efac62c14b0ba5606861e07b3e5867ae
Christoph Graczyk <graczyk@zib.de>
2022-10-28 16:16:14 +0200

    refactoring the interface

--- src/cutting_interface/cut_generator.h
+++ src/cutting_interface/cut_generator.h
@@ -17,6 +17,7 @@
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "ortools/base/status_macros.h"
 #include "ortools/base/logging.h"
 
 #include "src/data_structures/cuts_data.h"
@@ -25,100 +26,77 @@
 
 namespace minimip {
 
-// This datastructure should carry the data that is used by the generator to
-// iterate over or computes an iterative from. Can store the MipData adress and
-// LPInterface pointer by default.
-class SeparatorData{
+class SeparatorInterface{
  public:
-  virtual ~SeparatorData() = default;
-  MipData& mip_data = null_ptr;
-  LPInterface* lpi = null_ptr;
+  // Generate up to `max_num_cuts` cutting planes. Returns an empty vector if no
+  // cuts could be generated given the current state. If called multiple times,
+  // the generator is expected to continue where it left of, if appropriate.
+  virtual absl::StatusOr<std::vector<CuttingPlane>> GenerateCuttingPlanes(
+      const LPInterface* lpi, const MipData& mip_data, int max_num_cuts) = 0;
 };
 
-// The object that is used by the generator to compute the next cutting plane.
-class Iterative{
+class Separator : SeparatorInterface {
  public:
-  virtual ~Iterative() = default;
-};
-
-class CutGenerator {
- public:
-  virtual ~CutGenerator() = default;
-
-  // Indicate if this generator can be run in the current state. This is not a
-  // guarantee that `GenerateNextCuttingPlanes` will be able to find any cuts.
-  virtual bool MayBeRun(const LPInterface* lpi,
-                        const MipData& mip_data) const = 0;
-
-  // Extract the relevant data needed for the cut generation loop iteratives.
-  // Including the MipData adress and the LPInterface into the SepaData is fine
-  // if behaviour-dependent information is required for the generator.
-  // If this function is not implemented, it simply passes the MipData address
-  // as well as the LPI Pointer to the SeparatorData.
-  virtual absl::StatusOr<SeparatorData> InitializeSeparatorData(
-    const LPInterface* lpi, const MipData& mip_data, int max_num_cuts) = 0;
-
-  // Acts both as the while condition and the modification of the SepaData
-  // to allow refinement of information used to compute iteratives in the
-  // generation loop.
-  // If this function is not implemented, it simply returns true.
-  virtual bool PrepareNextIteration(SeparatorData SepaData) = 0;
-
-  // Construct the Iterative object from the SepaData that the generator acts on.
-  // Usually this would be a constraint or a column, depending on the generator.
-  virtual absl::StatusOr<Iterative> GetItFrom(const SeparatorData& SepaData) = 0;
-
-  // Modify the iterative used to generate the next cut, e.g. tightening constraints,
-  // modifying coefficients etc. If this function is not implemented, it simply
-  // returns the iterative.
-  virtual absl::StatusOr<Iterative> PreProcessing(Iterative It, const SeparatorData& SepaData) = 0;
-
-  // Computing the cutting plane from the current iterative and any additinal data needed.
-  virtual absl::StatusOr<CuttingPlane> ComputeCutFrom(Iterative It, const SeparatorData& SepaData) = 0;
-
-  // Modifying the newly extracted cutting plane, e.g. strengthening or sparsification.
-  // If this function is not implemented, it simply returns the cutting plane.
-  virtual absl::StatusOr<CuttingPlane> PostProcessing(CuttingPlane cut, const SeparatorData& SepaData) = 0;
-
-  // Apply a local selection heuristic to the generator specific cuts of the separation round.
-  // If this function is not implemented, it simply returns the vector of cuts.
-  virtual absl::StatusOr<std::vector<CuttingPlane>> LocalSelect(std::vector<CuttingPlane> cuts, int max_num_cuts) = 0;
+  virtual ~Separator() = default;
 
   // Generate up to `max_num_cuts` cutting planes. Returns an empty vector if no
   // cuts could be generated given the current state. If called multiple times,
   // the generator is expected to continue where it left of, if appropriate.
-  absl::StatusOr<std::vector<CuttingPlane>> GenerateCuttingPlanes(
-      const LPInterface* lpi, const MipData& mip_data, int max_num_cuts)
-      {
-        std::vector<CuttingPlane> cuts;
-        cuts.reserve(max_num_cuts);
+  absl::StatusOr<std::vector<CuttingPlane>> GenerateCuttingPlanes(const LPInterface* lpi, const MipData& mip_data, int max_num_cuts) final {
+    RETURN_IF_ERROR( PrepareSeparationRound(lpi, mip_data, max_num_cuts) );
 
-        absl::StatusOr<SeparatorData> SepaData = InitializeSeparatorData(lpi, mip_data, max_num_cuts);
-        if(SepaData.ok()) {
-          while( PrepareNextIteration(*SepaData) ) {
-            absl::StatusOr<Iterative> It = GetItFrom(*SepaData);
+    std::vector<CuttingPlane> cuts;
 
-            if(It.ok())
-              It = PreProcessing(*It, *SepaData);
+    while (MayBeRun(max_num_cuts)) {
+      RETURN_IF_ERROR(PrepareIteration());
 
-            if(!It.ok())
-              return It.status();
+      ASSIGN_OR_RETURN(std::vector<CuttingPlane> current_cuts, ComputeCuts());
 
-            absl::StatusOr<CuttingPlane> cut = ComputeCutFrom(*It, *SepaData);
+      RETURN_IF_ERROR(PostProcessing(current_cuts));
 
-            if(cut.ok())
-              cut = PostProcessing(*cut, *SepaData);
+      cuts.insert(cuts.end(), current_cuts.begin(), current_cuts.end());
+    }
 
-            if(!cut.ok())
-              return cut.status();
+    RETURN_IF_ERROR(LocallyFilter(cuts, max_num_cuts));
+    return cuts;
+  };
 
-            cuts.push_back(*cut);
-          }
-          return LocalSelect(cuts,max_num_cuts);
-        }
-        else
-          return SepaData.status();
-      };
+ protected:
+  // Extract the relevant data needed for the cut generation loop iteratives.
+  // Including the MipData address and the LPInterface into the Separator Data
+  // is fine if behaviour-dependent information is required for the generator.
+  // If this function is not implemented, it simply passes the MipData address
+  // as well as the LPI Pointer to the SeparatorData.
+  virtual absl::Status PrepareSeparationRound(
+      const LPInterface* lpi, const MipData& mip_data, int max_num_cuts) = 0;
+
+  // Indicate if this generator can be run in the current state. This is not a
+  // guarantee that `GenerateNextCuttingPlanes` will be able to find any cuts.
+  // TODO: Make sure the while loop is easy to exit.
+  virtual bool MayBeRun(int max_num_cuts) const {
+    return true;
+  }
+
+  // Modify the iterative used to generate the next cut, e.g. tightening constraints,
+  // modifying coefficients etc. If this function is not implemented, it simply
+  // returns the iterative.
+  virtual absl::Status PrepareIteration() = 0;
+
+  // Computing the cutting plane from the current iterative and any additional data needed.
+  virtual absl::StatusOr<std::vector<CuttingPlane>> ComputeCuts() = 0;
+
+  // Modifying the newly extracted cutting plane, e.g. strengthening or sparsification.
+  // If this function is not implemented, it simply returns the cutting plane.
+  virtual absl::Status PostProcessing(std::vector<CuttingPlane>& current_cuts) {
+    return absl::OkStatus();
+  }
+
+  // Apply a local selection heuristic to the generator specific cuts of the separation round.
+  // If this function is not implemented, it simply returns the vector of cuts.
+  virtual absl::Status LocallyFilter(std::vector<CuttingPlane>& cuts, int max_num_cuts) const {
+    return absl::OkStatus();
+    }
+
 };
 
 }  // namespace minimip
