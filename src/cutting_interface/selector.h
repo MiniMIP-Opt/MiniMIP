6150e7a2dad6d5dda5030510bd6f88b0335cc123
Christoph Graczyk <graczyk@zib.de>
2022-11-07 14:21:17 +0100

    minimum viable interface, needs solver main class

--- src/cutting_interface/selector.h
+++ src/cutting_interface/selector.h
@@ -19,9 +19,8 @@
 #include "absl/status/statusor.h"
 #include "ortools/base/logging.h"
 #include "ortools/base/status_macros.h"
-#include "src/data_structures/cuts_data.h"
-#include "src/data_structures/mip_data.h"
-#include "src/lp_interface/lpi.h"
+#include "src/data_structures/solver.h"
+
 
 namespace minimip {
 
@@ -32,64 +31,14 @@ namespace minimip {
 //      selector should be implemented here, allowing the user to turn off all
 //      cut selection strategies for testing or analytics.
 
-class SelectorInterface {
+class Selector {
  public:
-  // Generate up to `max_num_cuts` cutting planes. Returns an empty vector if no
+  // Select up to `max_num_cuts` cutting planes. Returns an empty vector if no
   // cuts could be generated given the current state. If called multiple times,
   // the generator is expected to continue where it left of, if appropriate.
-  virtual absl::StatusOr<int> SelectCuttingPlanes(
-      const LPInterface* lpi, const MipData& mip_data,
-      std::vector<CuttingPlane>& cuts) = 0;
-};
-
-class Selector : SelectorInterface {
- public:
-  virtual ~Selector() = default;
-
-  absl::StatusOr<int> SelectCuttingPlanes(
-      const LPInterface* lpi, const MipData& mip_data,
-      std::vector<CuttingPlane>& cuts) final {
-    RETURN_IF_ERROR(PrepareSelection(lpi, mip_data, cuts));
-
-    while (number_of_cuts_selected_ < max_number_of_cuts_to_select_) {
-      RETURN_IF_ERROR(PrepareIteration(cuts));
-
-      RETURN_IF_ERROR(Filtering(cuts));
-
-      RETURN_IF_ERROR(Scoring(cuts));
-      number_of_cuts_selected_++;
-    }
-
-    return number_of_cuts_selected_;
-  }
-
- protected:
-  // Extract the relevant data needed for the following cut selection loop.
-  // This must include some sort of pre-scoring notion, refreshing the current
-  // score of a cut.
-  // TODO: add "isCutFresh()" like function corresponding to its current_score.
-  virtual absl::Status PrepareSelection(
-      const LPInterface* lpi, const MipData& mip_data,
-      const std::vector<CuttingPlane>& cuts) const = 0;
-
-  // Prepare the next iteration of filtering cuts and any preprocessing needed
-  virtual absl::Status PrepareIteration(std::vector<CuttingPlane>& cuts) = 0;
-
-  // Computing the cutting plane from the current iterative and any additional
-  // data needed.
-  virtual absl::Status Filtering(std::vector<CuttingPlane>& cuts) = 0;
-
-  // Compute new scores after filtering if necessary.
-  virtual absl::Status Scoring(std::vector<CuttingPlane>& cuts) = 0;
-
- private:
-  double max_parallelism_between_cuts_ = 1.0;
-  double min_selection_threshold_ = 0.0;
-  int number_of_cuts_selected_ = 0;
-  int max_number_of_cuts_to_select_ = 0;
-  int current_number_of_cuts_ = 0;
-  char filtermode_ = 'd';
-  char scoringmode_ = 'd';
+  virtual absl::StatusOr<int> SelectCuttingPlanes(const Solver &solver,
+                                                  std::vector<CuttingPlane> &cuts,
+                                                  int max_num_cuts) = 0;
 };
 
 }  // namespace minimip
