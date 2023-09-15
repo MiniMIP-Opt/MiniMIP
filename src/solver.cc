4efcf70fcd4e3cd40f563913310ebb0631200d37
Christoph Graczyk <graczyk@zib.de>
2023-09-15 11:01:23 +0200

    cleanup draft

--- src/solver.cc
+++ src/solver.cc
@@ -47,27 +47,25 @@ absl::StatusOr<MiniMipResult> Solver::Solve() {
     NodeIndex current_node = node_deque.front();
     node_deque.pop_front();
 
-    // Apply the branching decisions and solve the LP relaxation in the current
-    // node
-    NodeData current_node_data = tree.node(current_node);
-    if (current_node_data.parent != kInvalidNode) {
-      // Apply branching decisions here based on currentNodeData
-      // For example, update the bounds of currentNodeData.branch_variable
-    }
-
 #ifdef CUTTING
     // Add CuttingPlane loop here to add cuts to the LP before branching.
 #endif
+
+    NodeData current_node_data = tree.node(current_node);
+    if (current_node_data.parent != kInvalidNode) {
+      // Apply branching decisions here based on current_node_data
+      // For example, update the bounds of current_node_data.branch_variable
+    }
     CHECK_OK(lp->SolveLPWithDualSimplex());  // Solve the LP
 
     if (!lp->IsOptimal()) {
-      // The node is infeasible, prune it
+      tree.CloseNodeAndReclaimNodesUpToRootIfPossible(current_node);
       continue;
     }
 
     // Update the objective value for the current node
     objective_value = lp->GetObjectiveValue();
-    current_node_data.lp_objective_value = objective_value;
+    tree.SetLpRelaxationDataInNode(current_node, objective_value);
 
     // Check if the solution is integral
     bool is_integral = true;
@@ -99,18 +97,6 @@ absl::StatusOr<MiniMipResult> Solver::Solve() {
           current_node, branching_variable, true, max_fractional_part);
       NodeIndex right_child = tree.AddNodeByBranchingFromParent(
           current_node, branching_variable, false, max_fractional_part);
-
-      // Update node data
-      NodeData left_node_data = tree.node(left_child);
-      NodeData right_node_data = tree.node(right_child);
-
-      left_node_data.branch_variable = branching_variable;
-      left_node_data.branch_down = true;
-      left_node_data.branch_primal_value_in_parent_lp = max_fractional_part;
-
-      right_node_data.branch_variable = branching_variable;
-      right_node_data.branch_down = false;
-      right_node_data.branch_primal_value_in_parent_lp = max_fractional_part;
 
       // Add the new nodes to the deque for further processing
       node_deque.push_front(left_child);
