390eb2505040498485af9a4e1da6f256574ac572
cgraczyk <graczyk@zib.de>
2024-03-06 14:12:00 +0100

    merge boilerplate update

--- unit_tests/solve_loop_test.cc
+++ unit_tests/solve_loop_test.cc
@@ -11,3 +11,21 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+
+#include "src/solver.h"
+
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+
+#include "absl/status/status.h"
+#include "ortools/base/status_macros.h"
+
+
+namespace minimip {
+
+
+
+} // minimip
