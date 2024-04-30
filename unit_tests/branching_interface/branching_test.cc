e2c3150ac759fbd4d640094c7a9f68ee0f5cac7c
Christoph Graczyk <graczyk@zib.de>
2024-04-30 15:41:20 +0200

    added branching interface unit tests

---
+++ unit_tests/branching_interface/branching_test.cc
@@ -0,0 +1,99 @@
+// Copyright (2024) the MiniMIP Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "src/branching_interface/branching_factory.h"
+#include "src/branching_interface/branching_interface.h"
+#include "src/branching_interface/random_branching.h"
+#include "src/parameters.pb.h"
+#include "src/solver.h"
+#include "unit_tests/utils.h"
+
+namespace minimip {
+
+// ==========================================================================
+// Branching Factory Tests
+// ==========================================================================
+
+TEST(BranchingFactoryTest, CreatesRandomBranchingWithValidParams) {
+  BranchingParameters params;
+  RandomBranchingParameters* random_params = params.mutable_random_branching_parameters();
+  random_params->set_random_seed(42);
+
+  absl::StatusOr<std::unique_ptr<BranchingInterface>> branching = CreateBranching(params);
+  EXPECT_TRUE(branching.ok());
+  EXPECT_TRUE(dynamic_cast<RandomBranching*>(branching.value().get()) != nullptr);
+}
+
+TEST(BranchingFactoryTest, FailsToCreateBranchingWithInvalidParams) {
+  BranchingParameters params; // Do not set random_branching_parameters
+  auto branching = minimip::CreateBranching(params);
+  EXPECT_FALSE(branching.ok());
+  EXPECT_EQ(branching.status().message(), "No branching-specific parameters set.");
+}
+
+// ==========================================================================
+// Random Branching Tests
+// ==========================================================================
+
+TEST(RandomBranchingTest, NextBranchingVariableWithoutIntegerVariables) {
+  BranchingParameters params;
+  RandomBranchingParameters* random_params = params.mutable_random_branching_parameters();
+  random_params->set_random_seed(42);
+
+  absl::StatusOr<std::unique_ptr<BranchingInterface>> branching = CreateBranching(params);
+
+  auto* random_branching = dynamic_cast<RandomBranching*>(branching.value().get());
+
+
+  ASSERT_OK_AND_ASSIGN(std::unique_ptr<Solver> solver, Solver::Create(MiniMipProblem()));
+
+  absl::StatusOr<ColIndex> branching_variable = random_branching->NextBranchingVariable(*solver);
+  EXPECT_FALSE(branching_variable.ok());
+  EXPECT_EQ(branching_variable.status().message(), "No integer variables to branch on.");
+
+}
+
+TEST(RandomBranchingTest, NextBranchingVariableReturnsValidVariable) {
+  BranchingParameters params;
+  RandomBranchingParameters* random_params = params.mutable_random_branching_parameters();
+  random_params->set_random_seed(42);
+
+  absl::StatusOr<std::unique_ptr<BranchingInterface>> branching = CreateBranching(params);
+
+  auto* random_branching = dynamic_cast<RandomBranching*>(branching.value().get());
+
+  MiniMipProblem problem;
+  problem.variables = std::vector<MiniMipVariable> {
+      MiniMipVariable{name: "1", is_integer: true},
+      MiniMipVariable{name: "2", is_integer: true},
+      MiniMipVariable{name: "3", is_integer: true},
+      MiniMipVariable{name: "4", is_integer: false},
+  };
+
+  ASSERT_OK_AND_ASSIGN(std::unique_ptr<Solver> solver, Solver::Create(problem));
+
+  absl::StatusOr<ColIndex> branching_variable = random_branching->NextBranchingVariable(*solver);
+  EXPECT_TRUE(branching_variable.ok());
+  EXPECT_LT(branching_variable.value(), ColIndex(4));
+  EXPECT_GE(branching_variable.value(), ColIndex(0));
+
+}
+// ==========================================================================
+// MaxFractional Branching Tests
+// ==========================================================================
+
+
+}
